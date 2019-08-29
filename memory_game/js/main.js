fcc9b226f4fa8daa3cd06291504895288ebcdd04
Matthew Sartain <matthewsartain@me.com>
2019-08-29 11:17:29 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,23 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+console.log("ready to go");
 
-console.log("user flipped " + cardOne);
-console.log("user flipped " + cardFour);
+const cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+
+
+var cardOne = cards [2];
+var cardTwo = cards [3];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
+
+if(cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]){
+	alert("you found a match!");
+} else {
+	alert("Sorry, try again.");
+}
+
+
