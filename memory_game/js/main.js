05ff41a442d1333b4c6421a50fa1d652a1459b9f
Matthew Sartain <matthewsartain@me.com>
2019-08-30 14:43:47 -0500

    created game  functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -4,20 +4,21 @@ const cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-
-var cardOne = cards [2];
-var cardTwo = cards [3];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-if(cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]){
-	alert("you found a match!");
-} else {
-	alert("Sorry, try again.");
+var checkForMatch = function(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  		console.log("You found a match!");
+	} else {
+  		console.log("Sorry, try again.");
+}
 }
 
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push([cardId]);
+	if(cardsInPlay.length === 2){
+	   checkForMatch();
+}
+}
 
+flipCard(0);
+flipCard(2);
