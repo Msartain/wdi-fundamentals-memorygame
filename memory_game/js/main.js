df9cc660ab47e81153bd22c262a0fe0ba2745460
Matthew Sartain <matthewsartain@me.com>
2019-09-02 22:05:28 -0500

    created card object, rank & suit

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,27 @@
 console.log("ready to go");
 
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+{	
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png"	
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png"	
+},
+];
 
 var cardsInPlay = [];
 
@@ -13,8 +34,11 @@ var checkForMatch = function(){
 }
 
 var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+
+	cardsInPlay.push(cards[cardId].rank);
 	if(cardsInPlay.length === 2){
 		checkForMatch();
 } 
@@ -22,4 +46,5 @@ var flipCard = function(cardId) {
 
 flipCard(0);
 flipCard(2);
+
 
